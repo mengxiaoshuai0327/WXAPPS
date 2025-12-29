90aae419926c19aec9c70dbdaa42f80361e2df90
mengxiaoshuai0327 <mengxiaoshuai1@163.com>
2025-12-29 12:10:02 +0800

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+示例 Python 程序
+"""
+
+def main():
+    print("Hello, GitHub!")
+    print("这是一个示例项目")
+
+if __name__ == "__main__":
+    main()
+
